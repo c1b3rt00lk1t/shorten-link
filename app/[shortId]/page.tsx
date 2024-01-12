b28f337d90b3fdcc7d3cc5e5f100fa8f461f047a
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-13 00:03:22 +0100

    Authomatic redirection if the link exists

--- app/[shortId]/page.tsx
+++ app/[shortId]/page.tsx
@@ -1,3 +1,20 @@
-export default function Page({ params }: { params: { shortId: string } }) {
-  return <div>My shortId: {params.shortId}</div>;
+import { redirect } from "next/navigation";
+import { getLinkToRedirect } from "../lib/actions";
+
+export default async function Page({
+  params: { shortId },
+}: {
+  params: { shortId: string };
+}) {
+  let url: string = "";
+  try {
+    const result = await getLinkToRedirect(shortId);
+    url = result.rows[0]?.url;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  if (!url) {
+    redirect("/");
+  }
+  redirect("https://" + url);
 }

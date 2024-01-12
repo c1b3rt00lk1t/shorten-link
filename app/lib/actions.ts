2f2ad15613a320a8600864199b6178b3dd77f632
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-12 17:59:12 +0100

    Insertion of original and shortened link pair

---
+++ app/lib/actions.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { sql } from "@vercel/postgres";
+
+export async function insertLinkToShorten(
+  originalUrl: string,
+  shortId: string
+) {
+  const query = sql`INSERT INTO shorten_link (url, short_id) VALUES (${originalUrl}, ${shortId})`;
+  const result = await query;
+  return result;
+}

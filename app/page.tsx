284b68ad2bfca8691cf517bf2fd51efdb4d7b968
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-12 01:57:22 +0100

    First basic main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
-}
+"use client";
+import { Typography, Col, Input, Row, Button, Space } from "antd";
+import styles from "./page.module.css";
+const { Compact } = Space;
+const { Title } = Typography;
+
+const Home = () => {
+  return (
+    <>
+      <Row justify="start" align="top" className={styles.firstHeightRow}>
+        <Col>
+          <Title level={2}>Beta version</Title>
+          <Title level={3}>Try at your own risk ;)</Title>
+        </Col>
+      </Row>
+      <Row justify="center" align="top" className={styles.secondHeightRow}>
+        <Col span={8}>
+          <Row justify="center" align="middle">
+            <Compact style={{ width: "100%" }}>
+              <Input
+                addonBefore="https://"
+                placeholder="Enter URL"
+                className={styles.urlInput}
+                size="large"
+              />
+              <Button size="large" type="primary">
+                Submit
+              </Button>
+            </Compact>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Home;

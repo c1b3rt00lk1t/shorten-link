92dd02919b204e2542f10717c87b6d437122b276
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-15 10:55:58 +0100

    Test it generates a short url than can be copied, shared, visited and cleared

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,12 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Short link app", () => {
+  it("generates a short url than can be copied, shared, visited and cleared", async () => {
     cy.visit("http://localhost:3000");
+
+    cy.findByRole("textbox").type("https://www.google.com");
+    cy.findByRole("button").click();
+    cy.findByRole("button", { name: "Copy to clipboard" }).click();
+    cy.findByRole("button", { name: "Share link" }).click();
+    cy.findByRole("button", { name: "Visit link" }).click();
+    cy.findByRole("button", { name: "Clear link" }).click();
   });
 });

694188c01eac1c94ff9fbd00a32f8070a727bad9
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-26 21:09:58 +0100

    Add metadata an viewport to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,56 @@ import NavBar from "./components/NavBar";
 import { blue } from "@ant-design/colors";
 import Footer from "./components/Footer";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// Added types for Next.js metadata and viewport
+import type { Metadata, Viewport } from "next";
+import type { ReactNode } from "react";
+
+// Metadata for PWA
+const APP_NAME = "Shorten links PWA";
+const APP_DEFAULT_TITLE = "Shorten links";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "PWA to shorten links and generate free QR codes";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={styles.reset}>
       <body

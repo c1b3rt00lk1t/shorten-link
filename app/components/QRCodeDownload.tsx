5de156576c7c7ec3a926df7b95be248631f28a9d
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-26 13:13:34 +0100

    Extract logic and styles for QR code in a component

---
+++ app/components/QRCodeDownload.tsx
@@ -0,0 +1,32 @@
+import { QRCode } from "antd";
+import styles from "./QRCodeDownload.module.css";
+
+type QRCodeDownloadProps = {
+  originalUrl: string;
+};
+
+const QRCodeDownload = ({ originalUrl }: QRCodeDownloadProps) => {
+  const downloadQRCode = () => {
+    const canvas = document
+      .getElementById("myqrcode")
+      ?.querySelector<HTMLCanvasElement>("canvas");
+    if (canvas) {
+      const url = canvas.toDataURL();
+      const a = document.createElement("a");
+      a.download = "QRCode.png";
+      a.href = url;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+  return (
+    <>
+      <div id="myqrcode" className={styles.myqrcode} onClick={downloadQRCode}>
+        <QRCode value={originalUrl || "-"} bgColor="white" />
+      </div>
+    </>
+  );
+};
+
+export default QRCodeDownload;

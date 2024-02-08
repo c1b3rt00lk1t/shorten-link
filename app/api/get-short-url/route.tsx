9f4f8f2907c666aa784a9b0b50b3c99b1f841ed6
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-02-08 12:51:40 +0100

    Add protocol to validation

--- app/api/get-short-url/route.tsx
+++ app/api/get-short-url/route.tsx
@@ -4,6 +4,9 @@ import { insertLinkToShorten } from "../../lib/actions";
 // URL validation utility
 const validateURL = (url: string) => {
   try {
+    if (!url.startsWith("https://") && !url.startsWith("http://")) {
+      url = "https://" + url;
+    }
     new URL(url);
     return true;
   } catch (error) {
